77f11f604098e192a609e70e2be990ed4945aa28
Eduardo Cintra dos Santos <eduardocintradossantos@gmail.com>
2025-04-23 15:08:54 -0300

    185 Ok

--- lib/infrastructure/datasources/moviedb_datasource.dart
+++ lib/infrastructure/datasources/moviedb_datasource.dart
@@ -1,6 +1,8 @@
 import 'package:cinemapedia/config/constants/environment.dart';
 import 'package:cinemapedia/domain/datasources/movies_datasource.dart';
 import 'package:cinemapedia/domain/entities/movie.dart';
+import 'package:cinemapedia/infrastructure/mappers/movie_mapper.dart';
+import 'package:cinemapedia/infrastructure/models/moviedb/moviedb_response.dart';
 import 'package:dio/dio.dart';
 
 class MoviedbDatasource extends MoviesDatasource{
@@ -8,7 +10,7 @@ class MoviedbDatasource extends MoviesDatasource{
   final dio = Dio(BaseOptions(
     baseUrl: 'https://api.themoviedb.org/3',
     queryParameters: {
-      'api-key': Environment.movieDbKey,
+      'api_key': Environment.movieDbKey,
       'language': 'pt-BR'
     }
   ));
@@ -18,8 +20,12 @@ class MoviedbDatasource extends MoviesDatasource{
   Future<List<Movie>> getNowPlaying({int page = 1}) async {
 
     final response = await dio.get('/movie/now_playing');
-    response.data;
-    final List<Movie> movies = [];
+    
+    final movieDBResponse = MovieDbResponse.fromJson(response.data);
+
+    final List<Movie> movies = movieDBResponse.results.map(
+      (moviedb) => MovieMapper.movieDBToEntity(moviedb)
+    ).toList();
 
     return movies;
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'package:cinemapedia/config/routes/app_router.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
@@ -9,7 +10,9 @@ Future<void> main() async{
 
   await dotenv.load(fileName: '.env');
 
-  runApp(const MyApp());
+  runApp(
+    const ProviderScope(child: MyApp()),
+  );
 }
 
 class MyApp extends StatelessWidget {
