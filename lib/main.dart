adc83e3bd276c223856609a39dc55ef13d3f3333
Eduardo Cintra dos Santos <eduardocintradossantos@gmail.com>
2025-04-23 11:04:20 -0300

    178 Ok

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
-import 'package:cinemapedia/config/routes/app_router.dart';
-import 'package:cinemapedia/config/theme/app_theme.dart';
 import 'package:flutter/material.dart';
+
+import 'package:cinemapedia/config/routes/app_router.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+import 'package:cinemapedia/config/theme/app_theme.dart';
 
 Future<void> main() async{
 
-  await dotenv.load(fileName: ".env");
+  await dotenv.load(fileName: '.env');
 
   runApp(const MyApp());
 }
--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:cinemapedia/config/constants/environment.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class HomeScreen extends StatelessWidget {
 
@@ -9,10 +10,8 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Scaffold(
-        body: Placeholder(),
-      ),
+    return const Scaffold(
+      body: Placeholder()
     );
   }
 }
